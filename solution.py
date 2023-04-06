966dd7d098e9a0081db463c88ff0911d5f67fd12
OlegTrushanin <83016212+OlegTrushanin@users.noreply.github.com>
2023-04-06 19:39:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,14 +8,20 @@ from scipy.stats import norm
 chat_id = 767458283 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    t_squared = 17 ** 2
+    s = x
     n = len(x)
+    y = s
+    x = np.ones((n, 2))
+    x[:, 1] = t_squared / 2
+    beta_hat = np.linalg.inv(x.T @ x) @ x.T @ y
+    a_hat = beta_hat[1]
+
+    # Строим доверительный интервал для коэффициента ускорения
     alpha = 1 - p
-    df = n - 1
-    t_val = t.ppf(1 - alpha / 2, df)
-    s = np.std(x, ddof=1)
-    mean_x = np.mean(x)
+    t_alpha_2 = t.ppf(1 - alpha / 2, n - 2)
+    se = np.sqrt(np.sum((y - x @ beta_hat) ** 2) / (n - 2))
+    ci_left = a_hat - t_alpha_2 * se / np.sqrt(np.sum((t_squared / 2 - np.mean(t_squared)) ** 2))
+    ci_right = a_hat + t_alpha_2 * se / np.sqrt(np.sum((t_squared / 2 - np.mean(t_squared)) ** 2))
 
-    left_bound = mean_x - t_val * s / np.sqrt(n) * np.sqrt(2 / 3)
-    right_bound = mean_x + t_val * s / np.sqrt(n) * np.sqrt(2 / 3)
-
-    return [left_bound, right_bound]
+    return ci_left, ci_right
