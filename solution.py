61cb1ef8795774cd6c96be30ffc11700a7b08f86
OlegTrushanin <83016212+OlegTrushanin@users.noreply.github.com>
2023-04-07 20:34:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,34 +1,27 @@
 import pandas as pd
 import numpy as np
 from scipy.optimize import minimize
-from scipy.stats import expon
-
+from scipy.stats import t, expon
 
 chat_id = 767458283 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    time = 17.0
+     # Количество измерений
+    n = len(x)
 
-    def negative_log_likelihood(acceleration):
-        predicted_distances = 0.5 * acceleration * time**2
-        errors = x - predicted_distances
-        return -np.sum(expon.logpdf(errors + 0.5, scale=1))
+    # Время эксперимента
+    t_exp = 17
 
-    # Оценка коэффициента ускорения с помощью метода максимального правдоподобия
-    mle_result = minimize(negative_log_likelihood, 9.8)
-    mle_acceleration = mle_result.x[0]
+    # Определение ускорения и его стандартной ошибки
+    a = 2 * x.mean() / (t_exp ** 2)
+    error = expon(scale=1/2).std()
+    se_a = 2 * error / (t_exp ** 2 * np.sqrt(n))
 
-    # Построение профиля правдоподобия для оценки доверительного интервала
-    alpha = 1 - p
-    log_likelihood_mle = -mle_result.fun
-    critical_value = expon.ppf(1 - alpha / 2)
+    # Вычисление границ доверительного интервала
+    df = n - 1
+    t_critical = t.ppf(1 - (1 - p) / 2, df)
+    lower = a - t_critical * se_a
+    upper = a + t_critical * se_a
 
-    def find_confidence_interval_bound(initial_guess, target_log_likelihood):
-        result = minimize(lambda acceleration: (negative_log_likelihood(acceleration) - target_log_likelihood)**2, initial_guess)
-        return result.x[0]
-
-    lower_bound = find_confidence_interval_bound(mle_acceleration - 1, log_likelihood_mle + critical_value)
-    upper_bound = find_confidence_interval_bound(mle_acceleration + 1, log_likelihood_mle + critical_value)
-
-    return lower_bound, upper_bound
+    return lower, upper
 
